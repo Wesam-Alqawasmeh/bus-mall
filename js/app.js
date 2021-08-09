3467a699692133a3cd9d90696c702b6c9bcd112b
Wesam-Alqawasmeh <alqawasmehwesam@gmail.com>
2021-08-09 14:47:06 +0300

    lab 11 edit

--- js/app.js
+++ js/app.js
@@ -31,6 +31,11 @@ let firstImage = document.getElementById('1stImage');
 let secondImage = document.getElementById('2ndImage');
 let thirdImage = document.getElementById('3rdImage');
 
+let firstRandom = 0;
+let secondRandom = 0;
+let thirdRandom = 0;
+
+
 const imageSection = document.getElementById('images-section');
 
 
@@ -39,6 +44,7 @@ function busMallPictures(name, imageSrc) {
     this.name = name;
     this.image = imageSrc;
     this.seen = 0;
+    this.clicked = 0;
     busMallPictures.all.push(this);
 
 }
@@ -53,9 +59,17 @@ console.log(busMallPictures.all);
 
 
 function run() {
-    let firstRandom = randomNumber(0, imageArr.length - 1);
-    let secondRandom = randomNumber(0, imageArr.length - 1);
-    let thirdRandom = randomNumber(0, imageArr.length - 1);
+    firstRandom = randomNumber(0, imageArr.length - 1);
+    
+    
+
+do {
+    secondRandom = randomNumber(0, imageArr.length - 1);
+} while (secondRandom == firstRandom)
+
+do {
+    thirdRandom = randomNumber(0, imageArr.length - 1);
+} while (thirdRandom == secondRandom || thirdRandom == firstRandom)
 
     firstImage.src = './img/' + busMallPictures.all[firstRandom].image;
     secondImage.src = './img/' + busMallPictures.all[secondRandom].image;
@@ -72,37 +86,51 @@ run();
 
 imageSection.addEventListener('click', clickHandler);
 function clickHandler(event) {
-    if ((event.target.id === '1stImage' || event.target.id === '2ndImage' || event.target.id === '3rdImage') && counter < round) {
+    if ((event.target.id === '1stImage' || event.target.id === '2ndImage' || event.target.id === '3rdImage') && counter < round) {   
         run();
         counter++;
-    }
-    if (counter == round) {
-        counter++;
 
-        
-        let section = document.getElementById('viewResult');
-        
-
-        let result = document.createElement('button');
-        result.textContent = 'Result';
-        section.appendChild(result);
-
-        result.addEventListener('click', clickListener);
-        function clickListener() {
-            let ulElement = document.createElement('ul');
-            section.appendChild(ulElement);
-
-            for (let i = 0; i < imageArr.length; i++) {
-                let liElement = document.createElement('li');
-                liElement.textContent = imageArr[i].split('.')[0] + ' picture has been seen ' + busMallPictures.all[i].seen + ' times';
-                ulElement.appendChild(liElement);
-            }
-
-
-
+        if (event.target.id === '1stImage') {
+            busMallPictures.all[firstRandom].clicked++;
         }
+
+        if (event.target.id === '2ndImage') {
+            busMallPictures.all[secondRandom].clicked++;
+        }
+
+        if (event.target.id === '3rdImage') {
+            busMallPictures.all[thirdRandom].clicked++;
+        }
+
+
     }
 }
+
+
+let section = document.getElementById('viewResult');
+
+
+let result = document.createElement('button');
+result.textContent = 'Result';
+section.appendChild(result);
+
+result.addEventListener('click', clickListener);
+function clickListener() {
+    let ulElement = document.createElement('ul');
+    section.appendChild(ulElement);
+
+    for (let i = 0; i < imageArr.length; i++) {
+        let liElement = document.createElement('li');
+        liElement.textContent = `${busMallPictures.all[i].name} has been seen ${busMallPictures.all[i].seen} times and voted ${busMallPictures.all[i].clicked} times.`;
+        ulElement.appendChild(liElement);
+    }
+
+    result.removeEventListener('click', clickListener);
+}
+
+
+
+
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
